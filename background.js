f4bd1abe1b3212775903afcc5f580decfa6a5a4d
Joe Hayes <Joe.Hayes@digital.ai>
2023-02-28 13:35:32 -0500

    First pass complete: Accessing current dom.

--- background.js
+++ background.js
@@ -1,26 +1,37 @@
+//Part 1 get dom access of current tab
+function ScrapeCurrentDOM() {
 
 
 
-function getAlert() { alert("Oh snap"); }
+}
 
 
-async function GetCurrentTabID() {
+async function GetAccessToCurrentDOM() {
 
     //set options for current/active tab, and retrieve it via extension API
     let queryOptions = { active: true, lastFocusedWindow: true };
 
     let [tab] = await chrome.tabs.query(queryOptions);
 
+    if(!tab) { return undefined; }
+    if (tab.url?.startsWith("chrome://")) {return undefined;}
+    //Part 2 validate if page is agility prior to scraping?
+    //TODO 
+
     chrome.scripting
     .executeScript({
       target : {tabId : tab.id},
-      func : getAlert,
+      func : ScrapeCurrentDOM,
     })
     .then(() => console.log("injected a function"));
 
 }
 
+GetAccessToCurrentDOM();
 
-GetCurrentTabID();
+//part 3 prep Description and Title for sending to API
+
+
+
 
 
